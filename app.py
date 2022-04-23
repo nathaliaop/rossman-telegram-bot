884c5b5bf6d63e8970fd055865688bf3c7c8a059
Nathália <np.nathaliapereira@gmail.com>
2022-04-23 19:48:07 -0300

    fix: Edita arquivos para teste

--- app.py
+++ app.py
@@ -1,43 +1,38 @@
 import re
 from flask import Flask, request
 import telegram
-
-import os
+from telebot.credentials import BOT_TOKEN, URL
 
 global bot
-global BOT_TOKEN
-BOT_TOKEN = os.environ.get('BOT_TOKEN')
-URL = os.environ.get('URL')
-bot = telegram.Bot(token=BOT_TOKEN)
+global TOKEN
+TOKEN = BOT_TOKEN
+bot = telegram.Bot(token=TOKEN)
 
 app = Flask(__name__)
 
-@app.route(f'/{BOT_TOKEN}'.format(BOT_TOKEN), methods=['POST'])
+@app.route('/{}'.format(TOKEN), methods=['POST'])
 def respond():
    # retrieve the message in JSON and then transform it to Telegram object
-  update = telegram.Update.de_json(request.get_json(force=True), bot)
+   update = telegram.Update.de_json(request.get_json(force=True), bot)
 
-  chat_id = update.message.chat.id
-  msg_id = update.message.message_id
+   chat_id = update.message.chat.id
+   msg_id = update.message.message_id
 
    # Telegram understands UTF-8, so encode text for unicode compatibility
-  text = update.message.text.encode('utf-8').decode()
+   text = update.message.text.encode('utf-8').decode()
    # for debugging purposes only
-  print("got text message :", text)
+   print("got text message :", text)
    # the first time you chat with the bot AKA the welcoming message
-  if text == "/start":
+   if text == "/start":
        # print the welcoming message
-      bot_welcome = """
-      Welcome to coolAvatar bot, the bot is using the service from http://avatars.adorable.io/ to generate cool looking avatars based on the name you enter so please enter a name and the bot will reply with an avatar for your name.
-      """
+       bot_welcome = """
+       Welcome to coolAvatar bot, the bot is using the service from http://avatars.adorable.io/ to generate cool looking avatars based on the name you enter so please enter a name and the bot will reply with an avatar for your name.
+       """
        # send the welcoming message
-      bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
+       bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
 
-  if text == "/predict":
-    url = "https://avatars.githubusercontent.com/nathaliaop"
-    bot.sendPhoto(chat_id=chat_id, photo=url, reply_to_message_id=msg_id)
-    pass
-    '''else:
+
+   else:
        try:
            # clear the message we got from any non alphabets
            text = re.sub(r"\W", "_", text)
@@ -48,22 +43,22 @@ def respond():
            bot.sendPhoto(chat_id=chat_id, photo=url, reply_to_message_id=msg_id)
        except Exception:
            # if things went wrong
-           bot.sendMessage(chat_id=chat_id, text="There was a problem in the name you used, please enter different name", reply_to_message_id=msg_id)'''
+           bot.sendMessage(chat_id=chat_id, text="There was a problem in the name you used, please enter different name", reply_to_message_id=msg_id)
 
-  return 'ok'
+   return 'ok'
 
 @app.route('/set_webhook', methods=['GET', 'POST'])
 def set_webhook():
-  bot_running = bot.setWebhook(f'https://api.telegram.org/bot{BOT_TOKEN}/setWebhook?url={URL}')
-  if bot_running:
-      return "Bot is running!"
-  else:
-      return "Something went wrong..."
+   s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
+   if s:
+       return "webhook setup ok"
+   else:
+       return "webhook setup failed"
 
 @app.route('/')
 def index():
-    return '.'
+   return '.'
+
 
 if __name__ == '__main__':
-  port = os.environ.get('PORT', 5000)
-  app.run(host='0.0.0.0', port=port)
+   app.run(threaded=True)
---
+++ telebot/credentials.py
@@ -0,0 +1,5 @@
+import os
+
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+BOT_USERNAME = os.environ.get('BOT_USERNAME')
+URL = os.environ.get('URL')
